ae442e71c6b863348ad9ef7f250038150a51f5c3
Gino-Freud-Hobayan <117270964+Gino-Freud-Hobayan@users.noreply.github.com>
2023-05-29 03:43:50 +0800

    Add files via upload

---
+++ 1. AAA - New Notebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello World!')
+
+
+# # First Notebook
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+copy
+
+
+# In[ ]:
+
+
+copy
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
